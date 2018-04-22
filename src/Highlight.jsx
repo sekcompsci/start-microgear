fe3e6565fbedbfaa5092b2e3d14ef9e8bb5a8814
Sirawit Moonrinta <niranam51150@hotmail.com>
2018-04-23 00:04:21 +0700

    Updates

--- src/Highlight.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { googlecode } from 'react-syntax-highlighter/styles/hljs'
-
-const Highlight = (lang, codeString) => {
-    return(<SyntaxHighlighter language={lang} style={googlecode}>{codeString}</SyntaxHighlighter>)
-}
-
-export default Highlight
