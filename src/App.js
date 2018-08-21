5faf53bdebff1d97deb5654891f1840046374957
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2018-08-21 12:22:03 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import { Layout, Form, Input, Tooltip, Icon, Button } from 'antd'
+import {Button, Form, Icon, Input, Layout, Select, Tooltip} from 'antd'
 import './App.css'
 
 import ESP8266 from './devices/esp8266'
 
-const { Sider, Content } = Layout;
+const {Sider, Content, Header} = Layout;
+const Option = Select.Option;
 const FormItem = Form.Item;
 
 class App extends React.Component {
@@ -20,27 +21,27 @@ class App extends React.Component {
 
     handleSSID = (e) => {
         const val = e.target.value;
-        this.setState({ ssid: val })
+        this.setState({ssid: val})
     };
 
     handlePASS = (e) => {
         const val = e.target.value;
-        this.setState({ pass: val })
+        this.setState({pass: val})
     };
 
     handleAPPID = (e) => {
         const val = e.target.value;
-        this.setState({ appid: val })
+        this.setState({appid: val})
     };
 
     handleKEY = (e) => {
         const val = e.target.value;
-        this.setState({ appkey: val })
+        this.setState({appkey: val})
     };
 
     handleSECRET = (e) => {
         const val = e.target.value;
-        this.setState({ appsecret: val })
+        this.setState({appsecret: val})
     };
 
     clearState = () => {
@@ -56,38 +57,39 @@ class App extends React.Component {
     };
 
     render() {
-        const { getFieldDecorator } = this.props.form;
+        const {getFieldDecorator} = this.props.form;
         const formItemLayout = {
             labelCol: {
-                xs: { offset: 1, span: 6 },
-                sm: { offset: 2, span: 20 }
+                xs: {offset: 1, span: 6},
+                sm: {offset: 2, span: 20}
             },
             wrapperCol: {
-                xs: { offset: 1, span: 6 },
-                sm: { offset: 2, span: 20 }
+                xs: {offset: 1, span: 6},
+                sm: {offset: 2, span: 20}
             },
         };
 
         return (
             <Layout className="layout">
                 <Sider breakpoint="md" width="350" collapsedWidth="0">
-                    <img className="logo" src="https://netpie.io/public/netpieio/img/logo/netpie_logo_4.png" alt="netpie" />
+                    <img className="logo" src="https://netpie.io/public/netpieio/img/logo/netpie_logo_4.png"
+                         alt="netpie"/>
                     <Form>
                         <FormItem
                             {...formItemLayout}
                             label={(
                                 <span>
                                     WIFI SSID &nbsp;
-                                        <Tooltip title="Wifi name">
-                                        <Icon type="question-circle-o" />
+                                    <Tooltip title="Wifi name">
+                                        <Icon type="question-circle-o"/>
                                     </Tooltip>
                                 </span>
                             )}
                         >
                             {getFieldDecorator('ssid', {
-                                rules: [{ required: true, message: 'Please input your ssid!', whitespace: true }],
+                                rules: [{required: true, message: 'Please input your ssid!', whitespace: true}],
                             })(
-                                <Input setfieldsvalue={this.state.ssid} onChange={this.handleSSID} />
+                                <Input setfieldsvalue={this.state.ssid} onChange={this.handleSSID}/>
                             )}
                         </FormItem>
                         <FormItem
@@ -95,16 +97,16 @@ class App extends React.Component {
                             label={(
                                 <span>
                                     WIFI PASSWORD &nbsp;
-                                        <Tooltip title="Wifi password">
-                                        <Icon type="question-circle-o" />
+                                    <Tooltip title="Wifi password">
+                                        <Icon type="question-circle-o"/>
                                     </Tooltip>
                                 </span>
                             )}
                         >
                             {getFieldDecorator('password', {
-                                rules: [{ required: true, message: 'Please input your password!', whitespace: true }],
+                                rules: [{required: true, message: 'Please input your password!', whitespace: true}],
                             })(
-                                <Input setfieldsvalue={this.state.pass} onChange={this.handlePASS} />
+                                <Input setfieldsvalue={this.state.pass} onChange={this.handlePASS}/>
                             )}
                         </FormItem>
                         <FormItem
@@ -112,16 +114,16 @@ class App extends React.Component {
                             label={(
                                 <span>
                                     NETPIE APPID &nbsp;
-                                        <Tooltip title="Application name from https://netpie.io/app">
-                                        <Icon type="question-circle-o" />
+                                    <Tooltip title="Application name from https://netpie.io/app">
+                                        <Icon type="question-circle-o"/>
                                     </Tooltip>
                                 </span>
                             )}
                         >
                             {getFieldDecorator('appid', {
-                                rules: [{ required: true, message: 'Please input your appid!', whitespace: true }],
+                                rules: [{required: true, message: 'Please input your appid!', whitespace: true}],
                             })(
-                                <Input setfieldsvalue={this.state.appid} onChange={this.handleAPPID} />
+                                <Input setfieldsvalue={this.state.appid} onChange={this.handleAPPID}/>
                             )}
                         </FormItem>
                         <FormItem
@@ -129,16 +131,16 @@ class App extends React.Component {
                             label={(
                                 <span>
                                     NETPIE KEY &nbsp;
-                                        <Tooltip title="string for authen netpie">
-                                        <Icon type="question-circle-o" />
+                                    <Tooltip title="string for authen netpie">
+                                        <Icon type="question-circle-o"/>
                                     </Tooltip>
                                 </span>
                             )}
                         >
                             {getFieldDecorator('key', {
-                                rules: [{ required: true, message: 'Please input your key!', whitespace: true }],
+                                rules: [{required: true, message: 'Please input your key!', whitespace: true}],
                             })(
-                                <Input setfieldsvalue={this.state.appkey} onChange={this.handleKEY} />
+                                <Input setfieldsvalue={this.state.appkey} onChange={this.handleKEY}/>
                             )}
                         </FormItem>
                         <FormItem
@@ -146,27 +148,38 @@ class App extends React.Component {
                             label={(
                                 <span>
                                     NETPIE SECRET &nbsp;
-                                        <Tooltip title="string for authen netpie">
-                                        <Icon type="question-circle-o" />
+                                    <Tooltip title="string for authen netpie">
+                                        <Icon type="question-circle-o"/>
                                     </Tooltip>
                                 </span>
                             )}
                         >
                             {getFieldDecorator('secret', {
-                                rules: [{ required: true, message: 'Please input your secret!', whitespace: true }],
+                                rules: [{required: true, message: 'Please input your secret!', whitespace: true}],
                             })(
-                                <Input setfieldsvalue={this.state.appsecret} onChange={this.handleSECRET} />
+                                <Input setfieldsvalue={this.state.appsecret} onChange={this.handleSECRET}/>
                             )}
                         </FormItem>
-                        <Button type="danger" style={{ marginLeft: '2rem' }} onClick={this.clearState} ghost>Clear</Button>
-                        <CopyToClipboard text={this.props.command}>
-                            <Button type="primary" style={{ float: 'right', marginRight: '2rem' }}>Copy</Button>
-                        </CopyToClipboard>
+                        <Button type="danger" style={{float: 'right', marginRight: '2rem'}} onClick={this.clearState}
+                                ghost>Clear</Button>
                     </Form>
                 </Sider>
                 <Layout>
+                    <Header style={{background: '#fff', height: '50px', padding: '10px 0'}}>
+                        <CopyToClipboard text={this.props.command}>
+                            <Button type="primary" style={{float: 'right', marginRight: '1rem'}}>Copy</Button>
+                        </CopyToClipboard>
+                        <Select style={{width: '220px', float: 'right', marginRight: '1rem'}} placeholder="Select another microgear">
+                            <Option value="esp8266">esp8266</Option>
+                            <Option value="html">html</Option>
+                            <Option value="python">python</Option>
+                            <Option value="nodeJS">nodeJS</Option>
+                            <Option value="nb-iot">nb-iot</Option>
+                        </Select>
+                    </Header>
                     <Content className="content">
-                        <ESP8266 ssid={this.state.ssid} pass={this.state.pass} appid={this.state.appid} appkey={this.state.appkey} appsecret={this.state.appsecret} />
+                        <ESP8266 ssid={this.state.ssid} pass={this.state.pass} appid={this.state.appid}
+                                 appkey={this.state.appkey} appsecret={this.state.appsecret}/>
                     </Content>
                 </Layout>
             </Layout>
